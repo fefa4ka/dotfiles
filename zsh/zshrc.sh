4f137e33204df9a499028de9a14136e8b29eb7c3
Alexander Kondratev <alex@nder.work>
2020-09-02 22:42:29 +0300

    deploy, submodules

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -151,4 +151,16 @@ function feh() {
     open -b "drabweb.macfeh" "$@"
 }
 
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/usr/local/opt/fzf/shell/key-bindings.zsh"
