e5b4c8d19f63f082b0ba8e9fc9475cb04edde741
Alexander Kondratev <alex@nder.work>
2025-04-30 10:43:07 +0300

    style: fix spacing in shell script formatting pattern

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -151,7 +151,7 @@ lsp.bashls.setup(default_setup)
 
 -- Shell script formatting with shfmt
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = {"*.sh", "*.bash", "*.zsh"},
+  pattern = { "*.sh", "*.bash", "*.zsh" },
   callback = function()
     local bufnr = vim.api.nvim_get_current_buf()
     local filename = vim.api.nvim_buf_get_name(bufnr)
